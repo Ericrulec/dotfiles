b97db320d37e9d1e373e01255a7eefc6e7f49312
Erik Jensson <erik@jensson.no>
2023-07-17 12:46:26 +0200

    fixing <c-i>

--- nvim/nvim/lua/craftzdog/base.lua
+++ nvim/nvim/lua/craftzdog/base.lua
@@ -5,7 +5,6 @@ vim.opt.encoding = 'utf-8'
 vim.opt.fileencoding = 'utf-8'
 
 vim.wo.number = true
-
 vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
@@ -24,10 +23,11 @@ vim.opt.smarttab = true
 vim.opt.breakindent = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
-vim.opt.wrap = false -- No Wrap lines
+vim.opt.wrap = false         -- No Wrap lines
 vim.opt.backspace = { 'start', 'eol', 'indent' }
 vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
 vim.opt.wildignore:append { '*/node_modules/*' }
+
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
--- nvim/nvim/lua/craftzdog/maps.lua
+++ nvim/nvim/lua/craftzdog/maps.lua
@@ -15,8 +15,11 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
 
--- New tab
 keymap.set('n', 'te', ':tabedit')
+keymap.set("n", "<C-I>", "<C-I>", { noremap = true })
+
+-- New tab
+
 -- Split window
 keymap.set('n', 'ss', ':split<Return><C-w>w')
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
